a54286d49459f8df24e9831dd612b374bbe8c685
HackerOrWottt <159930067+HackerOrWottt@users.noreply.github.com>
2024-08-04 12:50:52 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //will treat all JS code as a newer version
+
+//not valid since we are using node js , not browser
+//alert("Hello")
+
+
+//types of primitive dataTypes in Javascript
+let name= "Abhishek" // string
+let age = 19 // Number whose range is -> 2 to power 53
+let isLoggedIn= true // boolean
+// null --> representation of empty value eg. let state = null
+// undefined
--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,5 +1,17 @@
 const a= 123
 let employeeName= "Abhishek"
+let chocolate= 2
 
+/*
+prefer not to use var
+because of issue in blosk scope and functional scope
+*/
+
+//prints only one variable at a time
 console.log(a)
-console.table([a,employeeName])
+
+//prints all the given values in a tabular format
+console.table([a,employeeName,chocolate])
+
+chocolate = 10
+console.table([a,employeeName,chocolate])
