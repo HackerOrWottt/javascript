764b133ec0d2a072cb33f0029be8a8c02cd2d0fa
HackerOrWottt <159930067+HackerOrWottt@users.noreply.github.com>
2024-08-05 13:29:55 +0000

    operations in JS

---
+++ 01_basics/operations.js
@@ -0,0 +1,31 @@
+/*
+basic Maths Operations
+
+let value =3
+let negValue= - (value)
+
+console.log(negValue);
+
+console.log(2 + 2 ) //addition
+console.log(2 - 2 ) //subtraction
+console.log(2 * 2 ) //multiplication
+console.log(2 / 2 ) //division
+console.log(2 % 2 ) //remainder
+console.log(2 ** 2 ) //power
+
+*/
+
+// concadination in strings
+let str1 = "Hello"
+let str2 = " Abhishek"
+
+console.log(str1 + str2)
+
+console.log("1" + 2 ) // result --> 12
+console.log(1 + "2" ) //result --> 12
+console.log("1" + 2 + 2 ) // result --> 122
+console.log(1 + 2 + "2" ) //result --> 32
+
+
+console.log(+true) // result --> 1 ,  since (+) is used to convert it into boolean values and hence it converts true into boolean value 1
+console.log(+"")  // result --> 0 ,  since (+) is used to convert it into boolean values and hence it converts "" into boolean which is 0
