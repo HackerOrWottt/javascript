b79a69cbb8d43b3b2364982ee56678663e5fe31f
HackerOrWottt <159930067+HackerOrWottt@users.noreply.github.com>
2024-08-06 13:36:56 +0000

    non primitive dataTypes

---
+++ 01_basics/nonPrimitiveDataTypesBasics.js
@@ -0,0 +1,17 @@
+//Declaration of Arrays
+
+const NCER= [1 , 2, 3, 4 ,5 ,6 ] // arrays of integers/ numbers
+
+//Declaration of obects
+// Objects are always in key : value pair
+
+let myObj = {
+    name : "Abhishek",
+    rollNO : 60
+}
+
+//Declaration of functions
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
