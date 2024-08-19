9e08f61d428d45a33a626aa06522b684a334a777
HackerOrWottt <159930067+HackerOrWottt@users.noreply.github.com>
2024-08-19 13:19:10 +0000

    object study done

---
+++ 02_basics/object.js
@@ -0,0 +1,14 @@
+// two ways of declaring an objects
+
+// 1. as a literals --> Not a singleton , means multiple instances of an object is created ..
+
+const JsUser = {
+    name : "Abhishek",
+    age : 19 ,
+    location : "Pune"
+}
+
+// to access the object we have two ways 
+// console.log(JsUser.name);
+// console.log(JsUser["name"]);
+
