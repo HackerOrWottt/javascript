32d59d8fc4b7adef8215cf0767098168bd5bf0e6
Abhishek Varma <159930067+HackerOrWottt@users.noreply.github.com>
2024-08-28 13:51:49 +0000

    function done

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -5,3 +5,19 @@ function addTwoNUms(number1, number2){ // here parameters are declared
 }
 
 addTwoNUms(3,4) // here arguments are passed
+
+function addElementToMyCart(...num){ // take multiple arguments as an input array
+    return num 
+}
+
+//how to pass object in a function
+
+const myObj = {
+    username : 199
+}
+
+function handleObject(myObj){
+    console.log(`${myObj.username}`)
+}
+
+handleObject(myObj)
